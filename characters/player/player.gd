e039ea427c85e3b0522375d98bf2b0c7ee1f1020
Michal Kozubik <michal.kozubik@sap.com>
2025-08-24 21:59:54 +0200

    Player movement + tileset setup

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,14 +1,27 @@
 class_name Player
 extends CharacterBody2D
 
-@export var move_speed: float = 100.0
+@export var move_speed: float = 50.0
 
 func _ready() -> void:
 	pass
-
 
 func _process(delta: float) -> void:
 	var input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_vector * move_speed
 
+	animate()
 	move_and_slide()
+
+
+func animate():
+	if (velocity.x > 0):
+		$AnimatedSprite2D.play("move_right")
+	elif (velocity.x < 0):
+		$AnimatedSprite2D.play("move_left")
+	elif (velocity.y < 0):
+		$AnimatedSprite2D.play("move_up")
+	elif (velocity.y > 0):
+		$AnimatedSprite2D.play("move_down")
+	else:
+		$AnimatedSprite2D.stop()
