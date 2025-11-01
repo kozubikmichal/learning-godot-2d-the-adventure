871b67ddf437bd4163d0a24dffefc3c3cb7822a8
Michal Kozubik <michal.kozubik@sap.com>
2025-11-01 22:15:52 +0100

    pause + dialogue

--- npc/npc.gd
+++ npc/npc.gd
@@ -1,6 +1,10 @@
 extends StaticBody2D
 class_name NPC
 
+@export var dialogue_lines: Array[String] = []
+
+var dialogue_active_line: int = 0
+
 var can_interact: bool = false
 
 func _ready() -> void:
@@ -8,7 +12,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") && can_interact:
-		$CanvasLayer.visible = !$CanvasLayer.visible
+		handle_interaction()
 
 func enable_interaction() -> void:
 	can_interact = true
@@ -16,3 +20,18 @@ func enable_interaction() -> void:
 func disable_interaction() -> void:
 	can_interact = false
 	$CanvasLayer.visible = false
+
+
+func handle_interaction() -> void:
+	if dialogue_active_line < dialogue_lines.size():
+		$CanvasLayer.visible = true
+		get_tree().paused = true
+		handle_dialogue()
+	else:
+		dialogue_active_line = 0
+		$CanvasLayer.visible = false
+		get_tree().paused = false
+
+func handle_dialogue() -> void:
+	$CanvasLayer/DialogLabel.text = dialogue_lines[dialogue_active_line]
+	dialogue_active_line += 1
